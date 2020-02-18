28c7340760e0c777893b7e11fe078f1f54627600
Anna Dybas <a.m.dybas@gmail.com>
2020-02-18 17:07:17 -0500

    Done.

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -4,9 +4,24 @@ import movieData from './data.js'
 
 export default class MovieShowcase extends Component {
 
+  // generateMovieCards = () => {
+  //   console.log(movieData)
+  //   return movieData.map( (movieObj) => {
+  //     console.log(movieObj)
+  //     return <MovieCard data={movieObj} />
+  //   })
+  //   // map over your movieData array and return an array of the correct JSX
+  // }
+
   generateMovieCards = () => {
-    // map over your movieData array and return an array of the correct JSX
-  }
+		return movieData.map((movie, index) => <MovieCard
+		      key={index}
+		      title={movie.title}
+		      IMDBRating={movie.IMDBRating}
+		      genres={movie.genres}
+		      poster={movie.poster}
+		    />)
+	};
 
   render() {
     return (
